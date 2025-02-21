d2d5244aa6b538a4804c3f6bdd5b3c633fef55ce
Pau0701 <parias@unbosque.edu.co>
2025-02-21 19:57:14 +0000

    Codigo

---
+++ mini-cuento/src/app/nueva-pagina/nueva-pagina.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nueva-pagina',
+  imports: [],
+  templateUrl: './nueva-pagina.component.html',
+  styleUrl: './nueva-pagina.component.css'
+})
+export class NuevaPaginaComponent {
+
+}
